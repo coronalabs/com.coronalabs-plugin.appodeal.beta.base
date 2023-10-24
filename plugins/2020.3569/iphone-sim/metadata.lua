f7b384fcc15422969f428e72ae7322930c178bb6
Scott Harrison <scottrules44@gmail.com>
2023-10-24 16:55:11 -0500

    3.2 beta 1

--- plugins/2020.3569/iphone-sim/metadata.lua
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -3,8 +3,9 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'plugin_appodeal_base', "xml2", "z", 'APDAppodealAdExchangeAdapter' },
-		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", },
+		staticLibs = { 'plugin_appodeal_base', "xml2", "z" },
+		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", "AppodealMediationCore", "Protobuf", "OMSDK_Appodeal",
+	 	"StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVideoPlayer", "StackXML", "SwiftProtobuf"},
 		frameworksOptional = {"SafariServices", "AdSupport"},
 		usesSwift  = true,
 	},
