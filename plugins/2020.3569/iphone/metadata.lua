16121b111097461cba7b626594cb2acf1d95f09a
Scott Harrison <scottrules44@gmail.com>
2023-10-29 20:34:14 -0500

    Beta Fixes

--- plugins/2020.3569/iphone/metadata.lua
+++ plugins/2020.3569/iphone/metadata.lua
@@ -5,7 +5,7 @@ local metadata =
 		format = 'staticLibrary',
 		staticLibs = { 'plugin_appodeal_base', "xml2", "z" },
 		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", "AppodealMediationCore", "Protobuf", "OMSDK_Appodeal",
-	 	"StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVideoPlayer", "StackXML", "SwiftProtobuf"},
+	 	"StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "SwiftProtobuf"},
 		frameworksOptional = {"SafariServices", "AdSupport"},
 		usesSwift  = true,
 	},
