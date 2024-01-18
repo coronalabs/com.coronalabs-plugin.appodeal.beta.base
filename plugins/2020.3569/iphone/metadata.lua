00ef452b13a172aca400594461fe5447331125c3
Scott Harrison <scottrules44@gmail.com>
2024-01-18 15:25:18 -0600

    Update 3.2.1.0-beta.1

--- plugins/2020.3569/iphone/metadata.lua
+++ plugins/2020.3569/iphone/metadata.lua
@@ -4,8 +4,8 @@ local metadata =
 	{
 		format = 'staticLibrary',
 		staticLibs = { 'plugin_appodeal_base', "xml2", "z" },
-		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", "AppodealMediationCore", "Protobuf", "OMSDK_Appodeal",
-	 	"StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "SwiftProtobuf"},
+		frameworks = {"JavaScriptCore", "CoreBluetooth", "WebKit", "CoreTelephony", "Appodeal", "AppodealMediationCore", "OMSDK_Appodeal", "Protobuf",
+	 	"StackAnalytics", "StackConsentManager", "StackCrashReporter", "StackFoundation", "StackIABAssets", "StackModules", "StackMRAIDKit", "StackNASTKit", "StackOpenMeasure", "StackProductPresentation", "StackProtobuf", "StackRichMedia", "StackUIKit", "StackVASTAssets", "StackVASTKit", "StackVideoPlayer", "StackXML", "SwiftProtobuf", "UserMessagingPlatform"},
 		frameworksOptional = {"SafariServices", "AdSupport"},
 		usesSwift  = true,
 	},
